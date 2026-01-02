d67104920b69479ad6d136097b43c00ba6e67d3d
Abdoulrhman-Laila <abdoulrhmanlaila23@gmail.com>
2026-01-02 18:37:49 +0300

    edit files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,13 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
+const eslintConfig = defineConfig({
+  extends: "next/core-web-vitals", // استخدم extends مباشرة بدل الاستيراد
+  ignorePatterns: [
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
-  ]),
-]);
+  ],
+});
 
 export default eslintConfig;
